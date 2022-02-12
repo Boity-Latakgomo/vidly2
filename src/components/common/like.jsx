ce14d14b656489d4cb4ad7c89738716896e899a8
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-02-12 13:54:54 +0200

    added a heart in front of each movie

---
+++ src/components/common/like.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+//input:liked:boolean
+//output: onClick
+
+class Like extends Component {
+    
+    render() { 
+        return(<i class="fa fa-heart-o" aria-hidden="true"/>);
+    }
+}
+ 
+export default Like ;
