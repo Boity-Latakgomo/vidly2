698c9881459610f6521388073f130e71bbdd1aa7
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-02-15 14:10:46 +0200

    implemented sorting

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,13 +5,15 @@ import Pagination from "./common/pagination";
 import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import { paginate } from "../utils/paginate";
+import _ from "lodash";
 
    class Movies extends Component {
        state = {
            movies: [],
            genres: [],
            currentPage: 1,
-           pageSize: 4
+           pageSize: 4,
+           sortColumn: { path: 'title', order: 'asc'}
          };
 
          componentDidMount(){
@@ -37,19 +39,27 @@ import { paginate } from "../utils/paginate";
                      this.setState({ selectedGenre: genre, currentPage:1 });
                  };
                  handleSort = path => {
-                     console.log(path);
+                     const sortColumn = {...this.state.sortColumn};
+                     if (sortColumn.path === path)
+                     sortColumn.order = (sortColumn.order === "asc") ? "desc": "asc";
+                     else {
+                         sortColumn.path = path;
+                         sortColumn.order = "asc";
+                     }
+                     this.setState({ sortColumn });
                  };
 
        render() { 
            const { length: count } = this.state.movies;
-        const { pageSize, currentPage, selectedGenre, movies: allMovies } = this.state;
+        const { pageSize, currentPage, sortColumn, selectedGenre, movies: allMovies } = this.state;
            if (count ===0) 
            return <p>There are no movies in the database.</p>;
            const filtered = selectedGenre && selectedGenre._id
             ? allMovies.filter(m => m.genre._id === selectedGenre._id)
              : allMovies;
+          const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]); 
 
-           const movies = paginate(filtered, currentPage, pageSize);
+           const movies = paginate(sorted, currentPage, pageSize);
 
            return (
                <div className="row">
