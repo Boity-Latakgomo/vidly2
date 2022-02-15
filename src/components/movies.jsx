d22342a4e8033b7681e080d4f41b04de6056713a
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-02-15 15:56:34 +0200

    sorting-Moving Responsibility

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -38,14 +38,8 @@ import _ from "lodash";
                  handleGenreSelect = genre => {
                      this.setState({ selectedGenre: genre, currentPage:1 });
                  };
-                 handleSort = path => {
-                     const sortColumn = {...this.state.sortColumn};
-                     if (sortColumn.path === path)
-                     sortColumn.order = (sortColumn.order === "asc") ? "desc": "asc";
-                     else {
-                         sortColumn.path = path;
-                         sortColumn.order = "asc";
-                     }
+                 handleSort = sortColumn => {
+                     
                      this.setState({ sortColumn });
                  };
 
@@ -75,6 +69,7 @@ import _ from "lodash";
         <p>Showing {filtered.length} movies in the database.</p>
            <MoviesTable
             movies={movies} 
+            sortColumn={sortColumn}
             onLike={this.handleLike} 
             onDelete={this.handleDelete}
             onSort={this.handleSort}
